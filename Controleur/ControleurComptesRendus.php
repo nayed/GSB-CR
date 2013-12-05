d7a773f6bdaca3538dda05ea2caf51a0c4e556eb
nayed <nayed.saidali@gmail.com>
2013-12-05 17:45:39 +0100

    page d'ajout de compte rendu

--- Controleur/ControleurComptesRendus.php
+++ Controleur/ControleurComptesRendus.php
@@ -18,23 +18,29 @@ class ControleurComptesRendus extends ControleurSecurise {
         $this->compteRendu = new CompteRendu();
     }
 
-    // Affiche la liste des praticiens
+    // Affiche la liste des comptes rendus
     public function index() {
+        $compteRendus = $this->compteRendu->getComptesRendus();
+        $this->genererVue(array('compteRendus' => $compteRendus));
+    }
+    
+    // Affiche un compte rendu
+    public function ajout() {
         $praticiens = $this->praticien->getPraticiens();
         $this->genererVue(array('praticiens' => $praticiens));
     }
     
-        // Ajoute un compte rendu
+    // Ajoute un compte rendu
     public function ajouter() {
-        $idPraticien = $this->requete->getParametre("id");
+         $idPraticien = $this->requete->getParametre("id");
         $idVisiteur = $this->requete->getSession()->getAttribut("idVisiteur");
         $dateRapport = $this->requete->getParametre("date");
         $motif = $this->requete->getParametre("motif");
         $bilan = $this->requete->getParametre("bilan");
         $this->compteRendu->ajouterCompteRendu($idPraticien, $idVisiteur, $dateRapport, $motif, $bilan);
         
-        // Exécution de l'action par défaut pour réafficher la liste des billets
         $this->genererVue();
-        
     }
+    
+    
 }
--- Modele/CompteRendu.php
+++ Modele/CompteRendu.php
@@ -5,9 +5,19 @@ require_once 'Framework/Modele.php';
 // Services métier liés aux praticiens 
 class CompteRendu extends Modele {
 
-    public function ajouterCompteRendu($praticien, $idVisiteur, $dateRapport, $motif, $bilan) {
+    private $sqlCompteRendu = 'select pr.id_praticien, id_rapport, date_rapport, motif, bilan from rapport_visite rv join praticien pr on
+        rv.id_praticien = pr.id_praticien';
+    
+    public function ajouterCompteRendu($idPraticien, $idVisiteur, $dateRapport, $motif, $bilan) {
         $sql = 'insert into RAPPORT_VISITE(id_praticien, id_visiteur, date_rapport, motif, bilan)'
             . ' values(?, ?, ?, ?, ?)';
-        $this->executerRequete($sql, array($praticien, $idVisiteur, $dateRapport, $motif, $bilan));
+        $this->executerRequete($sql, array($idPraticien, $idVisiteur, $dateRapport, $motif, $bilan));
+    }
+    
+    // Affiche la liste des comptes rendus
+    public function getComptesRendus() {
+        $sql = $this->sqlCompteRendu . ' order by id_praticien';
+        $compteRendu = $this->executerRequete($sql);
+        return $compteRendu;
     }
 }
